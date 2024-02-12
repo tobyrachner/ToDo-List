f84598e3411a6162a85b10c98b54a91a6c747a4a
Toby Rachner <toby@rachner.com>
2024-02-12 16:44:03 -0600

    updated styling and small bugfix

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 console.log('Hello world')
 
 function addNote() {
-    const notes = document.getElementById('notes');
     const text = document.getElementById('add-text');
-    const note = document.createElement('div');
-    const input = document.createElement('input');
-    const remove = document.createElement('button');
+    if (text.value != '') {
+        const notes = document.getElementById('notes');
+        const note = document.createElement('div');
+        const inner = document.createElement('div');
+        const input = document.createElement('input');
+        const remove = document.createElement('button');
 
-    note.classList.add('note');
-    input.type = 'checkbox';
-    remove.classList.add('delete');
-    remove.setAttribute('onclick', "removeNote(this)");
-    remove.innerText = 'Remove';
+        note.classList.add('note');
+        input.type = 'checkbox';
+        input.classList.add('check')
+        remove.classList.add('delete');
+        remove.setAttribute('onclick', "removeNote(this)");
+        remove.innerText = 'Remove';
 
-    note.append(input, text.value, remove);
-    notes.append(note);
-    text.value = '';
+        inner.append(text.value, remove)
+        note.append(input, inner);
+        notes.append(note);
+        text.value = '';
+    }
 }
 
 function removeNote(note) {
-    note.parentElement.remove();
+    note.parentElement.parentElement.remove();
 };
